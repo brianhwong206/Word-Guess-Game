bbd15809430c26b5d95cca3ac940656abc6e4424
brianhwong206 <brianhwong@gmail.com>
2018-08-24 14:22:40 -0700

    fourth copy 20180824

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,39 +1,39 @@
-var wordBank = ["rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "goat", "monkey", "rooster", "dog", "pig"]
-var validInputs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var remainingValidInputs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var wordBank = ["rat", "ox", "tiger", "rabbit", "dragon", "snake", "horse", "sheep", "monkey", "rooster", "dog", "pig"]; // wordbank array
+var validInputs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", 
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; // array for valid user key inputs 
+remainingValidInputs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", 
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]; // array tracker for remaining valid inputs.
+var computerSelection = wordBank[Math.floor(Math.random() * wordBank.length)];  // Randomly selects a value from the wordbank.
+var selectionLength = computerSelection.length; // Determines the length of the computer selection.
+var answerArray = []; // array that will hold the word bank item with delimiters
+var finalAnswerArray= []; // array that will hold the word bank item without delimiters
 
-// Randomly selects a value from the wordbank.
-var computerSelection = wordBank[Math.floor(Math.random() * wordBank.length)]; 
-
-// Determines the length of the computer selection.
-var selectionLength = computerSelection.length;
-
-// Array is generated based on the length of the computer selection
-var answerArray = [];
-var finalAnswerArray= [];
 for (var i = 0; i < selectionLength; i++){
-    answerArray[i] = "-";
+    answerArray[i] = "-"; // enters array and for every letter within the selected word, swap it out with a "-"
 }
-finalAnswerArray = answerArray.join(""); 
+finalAnswerArray = answerArray.join("");  // removal of delimiters
 
+
+var finalAnswerString = finalAnswerArray.toString(); // converts computer selection answer array to string. 
 var winCount = 0;
 var loseCount = 0;
-var lettersRemaining = computerSelection.length;
-var userGuess;
+var lettersRemaining = computerSelection.length; 
+var userGuess; // stores the user key input
 var guessesRemaining = 10;
-var lettersGuessed = []; // letters guessed array
+var lettersGuessed = []; // array of letters guessed array
 var userGuessAlreadyInArray = lettersGuessed.includes(userGuess); // boolean check
+
 
 // Functions
 
 function win(){
     winCount++;
+    hintAnimal();
 }
 
 function lose(){
     loseCount++;
+    hintAnimal();
 }
 
 function reset(){
@@ -42,20 +42,227 @@ function reset(){
     selectionLength = computerSelection.length;
     answerArray = [];
     finalAnswerArray= [];
-    for (var i = 0; i < selectionLength; i++){
-        answerArray[i] = "-";
-    }
+        for (var i = 0; i < selectionLength; i++){
+            answerArray[i] = "-";
+        }
     finalAnswerArray = answerArray.join("");
     guessesRemaining = 10;
     lettersGuessed = [];
     userGuessAlreadyInArray= [];
+    userGuess = "";
     remainingValidInputs = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];  
+    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    document.querySelector("#rat").style.display = "none";
+    document.querySelector("#ox").style.display = "none";
+    document.querySelector("#tiger").style.display = "none";
+    document.querySelector("#rabbit").style.display = "none";
+    document.querySelector("#dragon").style.display = "none";
+    document.querySelector("#snake").style.display = "none";
+    document.querySelector("#horse").style.display = "none";
+    document.querySelector("#monkey").style.display = "none";
+    document.querySelector("#sheep").style.display = "none";
+    document.querySelector("#rooster").style.display = "none";
+    document.querySelector("#dog").style.display = "none";
+    document.querySelector("#pig").style.display = "none";
+    hintAnimal();
+}
+
+function hintAnimal(){
+
+switch (computerSelection) {
+case "rat":
+    document.querySelector("#rat").style.display = "block";
+    document.querySelector("#ox").style.display = "none";
+    document.querySelector("#tiger").style.display = "none";
+    document.querySelector("#rabbit").style.display = "none";
+    document.querySelector("#dragon").style.display = "none";
+    document.querySelector("#snake").style.display = "none";
+    document.querySelector("#horse").style.display = "none";
+    document.querySelector("#monkey").style.display = "none";
+    document.querySelector("#sheep").style.display = "none";
+    document.querySelector("#rooster").style.display = "none";
+    document.querySelector("#dog").style.display = "none";
+    document.querySelector("#pig").style.display = "none";
+    break;
+
+    case "ox":
+    document.querySelector("#rat").style.display = "none";
+    document.querySelector("#ox").style.display = "block";
+    document.querySelector("#tiger").style.display = "none";
+    document.querySelector("#rabbit").style.display = "none";
+    document.querySelector("#dragon").style.display = "none";
+    document.querySelector("#snake").style.display = "none";
+    document.querySelector("#horse").style.display = "none";
+    document.querySelector("#monkey").style.display = "none";
+    document.querySelector("#sheep").style.display = "none";
+    document.querySelector("#rooster").style.display = "none";
+    document.querySelector("#dog").style.display = "none";
+    document.querySelector("#pig").style.display = "none";
+    break;
+
+    case "tiger":
+    document.querySelector("#rat").style.display = "none";
+    document.querySelector("#ox").style.display = "none";
+    document.querySelector("#tiger").style.display = "block";
+    document.querySelector("#rabbit").style.display = "none";
+    document.querySelector("#dragon").style.display = "none";
+    document.querySelector("#snake").style.display = "none";
+    document.querySelector("#horse").style.display = "none";
+    document.querySelector("#monkey").style.display = "none";
+    document.querySelector("#sheep").style.display = "none";
+    document.querySelector("#rooster").style.display = "none";
+    document.querySelector("#dog").style.display = "none";
+    document.querySelector("#pig").style.display = "none";
+    break;
+
+    case "rabbit":
+    document.querySelector("#rat").style.display = "none";
+    document.querySelector("#ox").style.display = "none";
+    document.querySelector("#tiger").style.display = "none";
+    document.querySelector("#rabbit").style.display = "block";
+    document.querySelector("#dragon").style.display = "none";
+    document.querySelector("#snake").style.display = "none";
+    document.querySelector("#horse").style.display = "none";
+    document.querySelector("#monkey").style.display = "none";
+    document.querySelector("#sheep").style.display = "none";
+    document.querySelector("#rooster").style.display = "none";
+    document.querySelector("#dog").style.display = "none";
+    document.querySelector("#pig").style.display = "none";
+    break;
+
+    case "dragon":
+    document.querySelector("#rat").style.display = "none";
+    document.querySelector("#ox").style.display = "none";
+    document.querySelector("#tiger").style.display = "none";
+    document.querySelector("#rabbit").style.display = "none";
+    document.querySelector("#dragon").style.display = "block";
+    document.querySelector("#snake").style.display = "none";
+    document.querySelector("#horse").style.display = "none";
+    document.querySelector("#monkey").style.display = "none";
+    document.querySelector("#sheep").style.display = "none";
+    document.querySelector("#rooster").style.display = "none";
+    document.querySelector("#dog").style.display = "none";
+    document.querySelector("#pig").style.display = "none";
+    break;
+
+    case "snake":
+    document.querySelector("#rat").style.display = "none";
+    document.querySelector("#ox").style.display = "none";
+    document.querySelector("#tiger").style.display = "none";
+    document.querySelector("#rabbit").style.display = "none";
+    document.querySelector("#dragon").style.display = "none";
+    document.querySelector("#snake").style.display = "block";
+    document.querySelector("#horse").style.display = "none";
+    document.querySelector("#monkey").style.display = "none";
+    document.querySelector("#sheep").style.display = "none";
+    document.querySelector("#rooster").style.display = "none";
+    document.querySelector("#dog").style.display = "none";
+    document.querySelector("#pig").style.display = "none";
+    break;
+
+    case "horse":
+    document.querySelector("#rat").style.display = "none";
+    document.querySelector("#ox").style.display = "none";
+    document.querySelector("#tiger").style.display = "none";
+    document.querySelector("#rabbit").style.display = "none";
+    document.querySelector("#dragon").style.display = "none";
+    document.querySelector("#snake").style.display = "none";
+    document.querySelector("#horse").style.display = "block";
+    document.querySelector("#monkey").style.display = "none";
+    document.querySelector("#sheep").style.display = "none";
+    document.querySelector("#rooster").style.display = "none";
+    document.querySelector("#dog").style.display = "none";
+    document.querySelector("#pig").style.display = "none";
+    break;
+
+    case "monkey":
+    document.querySelector("#rat").style.display = "none";
+    document.querySelector("#ox").style.display = "none";
+    document.querySelector("#tiger").style.display = "none";
+    document.querySelector("#rabbit").style.display = "none";
+    document.querySelector("#dragon").style.display = "none";
+    document.querySelector("#snake").style.display = "none";
+    document.querySelector("#horse").style.display = "none";
+    document.querySelector("#monkey").style.display = "block";
+    document.querySelector("#sheep").style.display = "none";
+    document.querySelector("#rooster").style.display = "none";
+    document.querySelector("#dog").style.display = "none";
+    document.querySelector("#pig").style.display = "none";
+    break;
+
+    case "sheep":
+    document.querySelector("#rat").style.display = "none";
+    document.querySelector("#ox").style.display = "none";
+    document.querySelector("#tiger").style.display = "none";
+    document.querySelector("#rabbit").style.display = "none";
+    document.querySelector("#dragon").style.display = "none";
+    document.querySelector("#snake").style.display = "none";
+    document.querySelector("#horse").style.display = "none";
+    document.querySelector("#monkey").style.display = "none";
+    document.querySelector("#sheep").style.display = "block";
+    document.querySelector("#rooster").style.display = "none";
+    document.querySelector("#dog").style.display = "none";
+    document.querySelector("#pig").style.display = "none";
+    break;
+
+    case "rooster":
+    document.querySelector("#rat").style.display = "none";
+    document.querySelector("#ox").style.display = "none";
+    document.querySelector("#tiger").style.display = "none";
+    document.querySelector("#rabbit").style.display = "none";
+    document.querySelector("#dragon").style.display = "none";
+    document.querySelector("#snake").style.display = "none";
+    document.querySelector("#horse").style.display = "none";
+    document.querySelector("#monkey").style.display = "none";
+    document.querySelector("#sheep").style.display = "none";
+    document.querySelector("#rooster").style.display = "block";
+    document.querySelector("#dog").style.display = "none";
+    document.querySelector("#pig").style.display = "none";
+    break;
+
+    case "dog":
+    document.querySelector("#rat").style.display = "none";
+    document.querySelector("#ox").style.display = "none";
+    document.querySelector("#tiger").style.display = "none";
+    document.querySelector("#rabbit").style.display = "none";
+    document.querySelector("#dragon").style.display = "none";
+    document.querySelector("#snake").style.display = "none";
+    document.querySelector("#horse").style.display = "none";
+    document.querySelector("#monkey").style.display = "none";
+    document.querySelector("#sheep").style.display = "none";
+    document.querySelector("#rooster").style.display = "none";
+    document.querySelector("#dog").style.display = "block";
+    document.querySelector("#pig").style.display = "none";
+    break;
+
+    case "pig":
+    document.querySelector("#rat").style.display = "none";
+    document.querySelector("#ox").style.display = "none";
+    document.querySelector("#tiger").style.display = "none";
+    document.querySelector("#rabbit").style.display = "none";
+    document.querySelector("#dragon").style.display = "none";
+    document.querySelector("#snake").style.display = "none";
+    document.querySelector("#horse").style.display = "none";
+    document.querySelector("#monkey").style.display = "none";
+    document.querySelector("#sheep").style.display = "none";
+    document.querySelector("#rooster").style.display = "none";
+    document.querySelector("#dog").style.display = "none";
+    document.querySelector("#pig").style.display = "block";
+    break;
+    }
 }
 
 
-// This function is run whenever the user presses a key.
-document.onkeyup = function(event) {      
+
+
+// This operation is run whenever the user presses a key.
+
+hintAnimal();
+document.onkeyup = function(event) { 
+    {
+
+        
+
 
 // Determines which key was pressed.
 userGuess = event.key;
@@ -94,38 +301,49 @@ var letterIndex = validInputs.indexOf(userGuess);
     finalAnswerArray = answerArray.join(""); 
     }
 
-    else {alert("Please input a valid character from a-z")}
+    //else {alert("Please input a valid character from a-z")}
     
 
-    if ((lettersRemaining === 0) && (guessesRemaining > 0)){ // win scenario if letters remaining = 0 and remaining guess is greater than 0
+    if ((lettersRemaining === 0) && (guessesRemaining >= 0)){ // win scenario if letters remaining = 0 and remaining guess is greater than 0  
         win();
         reset();
     }
 
-    if ((lettersRemaining >0) && (guessesRemaining === 0)) {
+    if ((lettersRemaining > 0) && (guessesRemaining === 0)) {
         lose();
         reset();
     }
+}
 
 // html
 
-var html = 
+// var html = 
 
-"<p>Wins: " + winCount + "</p>" +
-"<p>Losses: " + loseCount + "</p>" +
-"<p>Computer Selection: " + computerSelection + "</p>" +
-//"<p>Presented Selection Final Array: " + answerArray + "</p>" +
-"<p>Number of Letters Remaining: " + lettersRemaining + "</p>" +
-"<p>Presented Selection Final Array: " + finalAnswerArray + "</p>" +
-//"<p>Letter Index: " + letterIndex + "</p>" +
-"<p>UserGuess: " + userGuess + "</p>" +
-"<p>Guesses Remaining: " + guessesRemaining + "</p>" +
-"<p>User Guesses So Far: " + lettersGuessed.join(", ") + "</p>" +
-"<p>Number of Letters Index: " + indexRemaining + "</p>" +
-"<p>Letters Remaining: " + remainingValidInputs.join(", ") + "</p>"
+// "<p>Wins: " + winCount + "</p>" +
+// "<p>Losses: " + loseCount + "</p>" +
+// "<p>Computer Selection: " + computerSelection + "</p>" +
+// //"<p>Presented Selection Final Array: " + answerArray + "</p>" +
+// "<p>Number of Letters Remaining: " + lettersRemaining + "</p>" +
+// "<p>Presented Selection Final Array: " + finalAnswerArray + "</p>" +
+// //"<p>Letter Index: " + letterIndex + "</p>" +
+// "<p>UserGuess: " + userGuess + "</p>" +
+// "<p>Guesses Remaining: " + guessesRemaining + "</p>" +
+// "<p>User Guesses So Far: " + lettersGuessed.join(", ") + "</p>" +
+// "<p>Number of Letters Index: " + indexRemaining + "</p>" +
+// "<p>Letters Remaining: " + remainingValidInputs.join(", ") + "</p>"
 
 
 // Set the inner HTML contents of the #game div to our html string
-document.querySelector("#game").innerHTML = html;
+//document.querySelector("#game").innerHTML = html;
+
+
+document.querySelector("#winCount").innerHTML = "Wins: " + winCount;
+document.querySelector("#lossCount").innerHTML = "Losses: " + loseCount;
+document.querySelector("#answer").innerHTML = "Answer: " + finalAnswerArray;
+document.querySelector("#guessesRemaining").innerHTML = "Guesses Remaining: " + guessesRemaining;
+document.querySelector("#currentInput").innerHTML = "Your Current Guess: " + userGuess;
+document.querySelector("#guessesSoFar").innerHTML = "Letters Guessed: " + lettersGuessed.join(", ");
+document.querySelector("#userRemainingChoices").innerHTML = "Letters Remaining: " + remainingValidInputs.join(", ");
+console.log(computerSelection);
 
 }
